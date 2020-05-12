32a4a3f01a8fa3cfaab1778574fc5c3efb3770d8
Aleksandr Ivakin <pwian@mail.ru>
2020-05-12 18:47:09 +0300

    lesson 1

--- src/javaFundamental/Main.java
+++ src/javaFundamental/Main.java
@@ -3,6 +3,12 @@ package javaFundamental;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("hello Java");
+        System.out.println(42);
+        System.out.println("\n\tjava " + 11);
+        int i = 10;
+        System.out.println(i);
+        System.exit(2);
+        System.out.println(3);
     }
 }
